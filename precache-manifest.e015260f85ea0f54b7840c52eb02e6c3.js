e0af197d02359a544543e7568886b39b800f0a30
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2019-05-12 12:30:18 +0200

    Updates

---
+++ precache-manifest.e015260f85ea0f54b7840c52eb02e6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773db5547a26eeaa79e7af1c1ba2a38a",
+    "url": "/albumklubben/index.html"
+  },
+  {
+    "revision": "a42cac043d24911f4290",
+    "url": "/albumklubben/static/css/main.487d7ed6.chunk.css"
+  },
+  {
+    "revision": "198ff0d151f8cb0b34ea",
+    "url": "/albumklubben/static/js/2.dc04e0a4.chunk.js"
+  },
+  {
+    "revision": "a42cac043d24911f4290",
+    "url": "/albumklubben/static/js/main.c2a05f6a.chunk.js"
+  },
+  {
+    "revision": "56ad97f766d8acf61be6",
+    "url": "/albumklubben/static/js/runtime~main.282a29af.js"
+  }
+]);
